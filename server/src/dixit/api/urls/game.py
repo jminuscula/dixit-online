899a40f8ec9100b65f1aac0e6ea9ac850d3f534b
Jacobo Tarragón Cros <jacobo.tarragon@gmail.com>
2017-05-27 17:14:23 +0200

    arrenge API app contents

---
+++ server/src/dixit/api/urls/game.py
@@ -0,0 +1,31 @@
+
+from django.conf.urls import url
+
+from dixit.api.views.game import game
+from dixit.api.views.game import player
+from dixit.api.views.game import round
+
+
+urlpatterns = [
+    url(r'^game/$',
+        game.GameList.as_view(), name='game-list'),
+    url(r'^game/(?P<game_pk>[0-9]+)/$',
+        game.GameRetrieve.as_view(), name='game-detail'),
+
+    url(r'^game/(?P<game_pk>[0-9]+)/player/$',
+        player.PlayerList.as_view(), name='player-list'),
+    url(r'^game/(?P<game_pk>[0-9]+)/player/(?P<player_number>[0-9]+)/$',
+        player.PlayerRetrieve.as_view(), name='player-detail'),
+
+    url(r'^game/(?P<game_pk>[0-9]+)/round$',
+        round.RoundList.as_view(), name='round-list'),
+    url(r'^game/(?P<game_pk>[0-9]+)/round/(?P<round_number>[0-9]+)$',
+        round.RoundRetrieve.as_view(), name='round-detail'),
+    url(r'^game/(?P<game_pk>[0-9]+)/round/(?P<round_number>[0-9]+)/play$',
+        round.PlayList.as_view(), name='play-list'),
+    url(r'^game/(?P<game_pk>[0-9]+)/round/(?P<round_number>[0-9]+)/provide$',
+        round.PlayProvideCreate.as_view(), name='play-provide'),
+    url(r'^game/(?P<game_pk>[0-9]+)/round/(?P<round_number>[0-9]+)/vote$',
+        round.PlayVoteCreate.as_view(), name='play-vote'),
+
+]
