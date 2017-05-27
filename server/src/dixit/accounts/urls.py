bd1d2022c5ee7bef1a137b71fb9ef4521cbb45ae
Jacobo Tarragón Cros <jacobo.tarragon@gmail.com>
2017-05-27 17:05:44 +0200

    remove accounts app for now

--- server/src/dixit/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.conf.urls import url, include
-from django.contrib import admin
-
-
-urlpatterns = [
-    url(r'^', include('django.contrib.auth.urls')),
-]
--- server/src/dixit/urls.py
+++ server/src/dixit/urls.py
@@ -5,7 +5,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-
-    url(r'^accounts/', include('dixit.accounts.urls')),
     url(r'^api/', include('dixit.api.urls')),
 ]
