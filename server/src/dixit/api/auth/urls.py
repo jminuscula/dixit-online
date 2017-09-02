f0d681ae7db9374def747480caf701835a8878d7
Jacobo Tarragón Cros <jacobo.tarragon@gmail.com>
2017-09-02 13:31:35 +0200

    user games endpoint

--- server/src/dixit/api/auth/urls.py
+++ server/src/dixit/api/auth/urls.py
@@ -2,11 +2,12 @@
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token
 
-from dixit.api.auth.views.user import Me
+from dixit.api.auth.views.user import Me, UserGames
 
 
 urlpatterns = [
-    url(r'^me/', Me.as_view()),
+    url(r'^me/$', Me.as_view()),
+    url(r'^me/games$', UserGames.as_view()),
 
-    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-auth/$', obtain_jwt_token),
 ]
--- server/src/dixit/api/auth/views/user.py
+++ server/src/dixit/api/auth/views/user.py
@@ -1,13 +1,11 @@
 
-# from django.db import IntegrityError
-# from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-# from rest_framework.exceptions import NotFound
-# from rest_framework.response import Response
-from rest_framework import generics  # , status
 
+from dixit.game.models import Game
+from dixit.api.game.serializers.game import GameListSerializer
 from dixit.api.auth.serializers.user import UserSerializer
 
 
@@ -23,3 +21,20 @@ class Me(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class UserGames(generics.ListAPIView):
+    """
+    Retrieves games where the user is or has participated
+    """
+
+    serializer_class = GameListSerializer
+
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        qs = Game.objects.filter(players__user=self.request.user)
+        status = self.request.query_params.get('status', None)
+        if status is not None:
+            return qs.filter(status=status)
+        return qs
